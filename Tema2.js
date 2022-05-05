1db0a6895dc92e3c5475cd6626bb2681a3e54d6e
FarcasanuMelisa06 <79477669+FarcasanuMelisa06@users.noreply.github.com>
2022-05-05 18:13:55 +0300

    Update Tema2.js

--- Tema2.js
+++ Tema2.js
@@ -7,25 +7,22 @@ function problem1(number) {
         return number - 2;
 }
 
-
 //problem 2
 const savePrimes = primes => {
-    //console.log(savePrimes[1]);
-    const arrayPrimes = new Array;
-    let dimension = primes.length;
-    let contor = 0;
-
-    for (let i = 0; i < dimension; i++) {
+    const arrayPrimes = [];
+    for (let i = 0; i < primes.length; i++) {
         let prim = true;
         if (primes[i] < 2)
             continue;
         for (let div = 2; div < primes[i]; div++) {
-            if (primes[i] % div == 0)
+            if (primes[i] % div == 0) {
                 prim = false;
+                break;
+            }
 
         }
         if (prim) {
-            arrayPrimes[contor++] = primes[i];
+            arrayPrimes.push(primes[i]);
         }
     }
     console.log(arrayPrimes);
@@ -38,46 +35,32 @@ savePrimes(arr);
 //problem 3 (IIFE)
 const saveStrings = [1, '1', 'salut', 2, 6, true, 'true'];
 ((arrStrings) => {
-    const justStrings = new Array;
-    let dim = arrStrings.length;
-    let k = 0;
+    const justStrings = [];
 
-        arrStrings.forEach(item => {
-            if(typeof(item) === 'string') 
-              justStrings[k++] = item;
-        });
-        console.log(justStrings);
-        
+    arrStrings.forEach(item => typeof (item) === 'string'
+        && justStrings.push(item));
+    console.log(justStrings);
+
 })(saveStrings);
 
 
 //problem 4 (arrow function)
 const sum = SumOfDigits => {
-    Number(SumOfDigits); //convert strings to number
-    //console.log(SumOfDigits);
+    // console.log(typeof SumOfDigits)
     let sumImpar = 0;
-    let contor = 0;
-    let lengthDigits = SumOfDigits;
-
-    //size of string
-    while (lengthDigits != 0) {
-        contor++;
-        lengthDigits = lengthDigits / 10 | 0; //remove the last digit
-    }
-
-    for (let i = 0; i <= contor; i++) {
-        if ((SumOfDigits % 10) % 2 == 0) {
-            SumOfDigits = SumOfDigits / 10 | 0;
+    for (let i = 0; i < SumOfDigits.length; i++) {
+        if (Number(SumOfDigits[i]) % 2 == 0)
             continue;
-        }
         else
-            sumImpar = sumImpar + (SumOfDigits % 10);
-        SumOfDigits = SumOfDigits / 10 | 0;
+            sumImpar = sumImpar + Number(SumOfDigits[i]);
     }
+
     console.log(sumImpar);
+    //console.log(typeof SumOfDigits)
 
 }
 sum("1978");
+
 
 // //problem 5 (switch)
 const FlowerColor = "yellow";
