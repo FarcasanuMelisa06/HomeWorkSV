5a17a34fe4f5facf4f54c9c0c002e182465993a7
Melisa Farcasanu <melisa_farcasanu@yahoo.com>
2022-05-21 11:38:00 +0300

    Update 2 - Tema 3

--- FirstReactApp/src/components/example3.js
+++ FirstReactApp/src/components/example3.js
@@ -4,55 +4,54 @@ import Colors from "./Colors";
 
 const Example3 = () => {
   const [colorCode, setColor] = useState([]);
- 
-    const addColorsCode = (newColor) => {
-      const newColors = [...colorCode];
-      newColor.id = colorCode.length;
-      newColors.push(newColor);
-      setColor(newColors);
-    };
 
-    const removeColor = (id) => {
-      const updateColors = colorCode.filter((removeColor) => 
+  const addColorsCode = (newColor) => {
+    newColor.id = colorCode.length;
+    setColor([...colorCode, newColor]);
+  };
+
+
+  const removeColor = (id) => {
+    const updateColors = colorCode.filter((removeColor) =>
       removeColor.id !== id);
-      setColor(updateColors);
-    };
+    setColor(updateColors);
+  };
 
-    const setGradientColor = () => {
-      const updateColors = colorCode
-        .map((newColor) => newColor.colorCode)
-        .toString();
-      switch (colorCode.length) {
-        case 0:
-          return colorCode;
-        case 1:
-          return updateColors;
-        default:
-          return `linear-gradient(${colorCode})`;
-      }
-    };
+  const setGradientColor = () => {
+    const updateColors = colorCode
+    .map((newColor) => newColor.color)
+      .toString();
+    switch (colorCode.length) {
+      case 0:
+        return colorCode;
+      case 1:
+        return updateColors;
+      default:
+        return `linear-gradient(${updateColors})`;
+    }
+  };
 
-return (
-  <div className="example">
-    Example 3 - Add button
-    <br/>
-    <AddButton handleClick= {addColorsCode}/>
-    <br/>
-    <div className="d-flex">
-      <div className="containerGradient">
-      <span>Gradient Result</span>
-      <div className="gradient-box"
-            style={{ background: `${setGradientColor()}`}}
+  return (
+    <div className="example">
+      Example 3 - Add button
+      <br />
+      <AddButton handleClick={addColorsCode} />
+      <br />
+      <div className="d-flex">
+        <div className="containerGradient">
+          <span>Gradient Result</span>
+          <div className="gradient-box"
+            style={{ background: `${setGradientColor()}` }}
           ></div>
-      </div>
-      {colorCode.map((colorId) => (
-          <Colors key = {colorId.id} {...colorId}
-          handleRemove={removeColor}/>
+        </div>
+        {colorCode.map((colorId) => (
+          <Colors key={colorId.id} {...colorId}
+            handleRemove={removeColor} />
 
         ))}
+      </div>
     </div>
-  </div>
-);
+  );
 
 };
 
